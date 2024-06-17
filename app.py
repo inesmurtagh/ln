e6ee05cec48ee274d3d0a550b97dfe136c6b0780
Ines Murtagh <88330001+inesmurtagh@users.noreply.github.com>
2024-06-16 23:21:51 -0300

    Update app.py

--- app.py
+++ app.py
@@ -324,7 +324,7 @@ def mostrar_noticia(categoria, autor, titulo_rec, subtitulo_rec, tono_rec, pregu
                 <p style='font-size: 18px;'>Incluir pregunta: {pregunta_rec}</p>
                 <p style='font-size: 16px; color: grey;'>Escrita por: {autor}</p>
             </div>
-            <img src='https://raw.githubusercontent.com/inesmurtagh/ln/main/images/portada.jpg' style='width: 300px; height: auto; position: absolute; right: 20px; top: 50px;'>
+            <img src='https://raw.githubusercontent.com/inesmurtagh/ln/main/images/portada.jpg' style='width: 300px; height: auto; position: absolute; right: 20px; top: 70px;'>
         </div>
         """, unsafe_allow_html=True
     )
