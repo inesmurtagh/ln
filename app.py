dade461941f638b62a76439740b861b627669279
Ines Murtagh <88330001+inesmurtagh@users.noreply.github.com>
2024-06-16 23:43:01 -0300

    Update app.py

--- app.py
+++ app.py
@@ -319,11 +319,12 @@ def mostrar_noticia(categoria, autor, titulo_rec, subtitulo_rec, tono_rec, pregu
             <div style="border-bottom: 2px solid black; margin-bottom: 1px;">
                 <p style='font-size: 18px; color: black; font-weight: bold;'>{categoria.upper()}</p>
             </div>
-            <div style="padding-top: 5px; line-height: 1.2;">
+            <div style="padding-top: 5px; line-height: 1;">
                 <h2 style='font-size: 24px; margin-bottom: 0;'>Titulo: {titulo_rec}</h2>
                 <h3 style='font-size: 20px; color: grey; margin-top: 0;'>Subtitulo: {subtitulo_rec}</h3>
                 <p style='font-size: 16px; color: white;'>espacio</p>
                 <p style='font-size: 16px; color: black;'>Agregar un tono {tono_rec}, {retorica} pregunta retórica</p>
+                <p style='font-size: 16px; color: white;'>espacio</p>
                 <p style='font-size: 16px; color: grey;'>Escrita por: {autor}</p>
             </div>
             <img src='https://raw.githubusercontent.com/inesmurtagh/ln/main/images/portada.jpg' style='width: 200px; height: 200px; object-fit: cover; position: absolute; right: 20px; top: 70px;'>
